cd2c53f61c48a7d99732e50f9f12da2c65fa1e3b
Jeppe Klitgaard <hi@jeppe.science>
2026-04-18 00:39:53 +0200

    fix: re-export private import for type checkers
    Since imports are private by convention in Python, we re-export to demarcate them as public.
    
    This prevents pylance and other type checkers from complaining.

--- jax_tqdm/__init__.py
+++ jax_tqdm/__init__.py
@@ -1,3 +1,3 @@
-from jax_tqdm.base import PBar
-from jax_tqdm.loop_pbar import loop_tqdm
-from jax_tqdm.scan_pbar import scan_tqdm
+from jax_tqdm.base import PBar as PBar
+from jax_tqdm.loop_pbar import loop_tqdm as loop_tqdm
+from jax_tqdm.scan_pbar import scan_tqdm as scan_tqdm
